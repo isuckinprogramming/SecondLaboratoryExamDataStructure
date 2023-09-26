63e5e7b84857cfbd5dc54f5c7114b52d4aee4b13
Borgs <nisiyawa55@gmail.com>
2023-09-25 18:45:37 -0700

    Copied the code from the original to custom
    The copied functions from the original, most of which are static, is converted to non-static inside the custom expression tree class.

--- src/LimModifiedExpressionTree/CustomExpressionTree.java
+++ src/LimModifiedExpressionTree/CustomExpressionTree.java
@@ -1,5 +1,83 @@
 package LimModifiedExpressionTree;
 
+import java.util.Stack;
+
 public class CustomExpressionTree {
 
+    // Function to check if a given token is an operator
+    public boolean isOperator(char c) {
+        return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
+    }
+
+    // Print the postfix expression for an expression tree
+    public void postorder(CustomNode root)
+    {
+        if (root == null) {
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data);
+    }
+
+    // Print the infix expression for an expression tree
+    public void inorder(CustomNode root)
+    {
+        if (root == null) {
+            return;
+        }
+
+        // if the current token is an operator, print open parenthesis
+        if (isOperator(root.data)) {
+            System.out.print("(");
+        }
+
+        inorder(root.left);
+        System.out.print(root.data);
+        inorder(root.right);
+
+        // if the current token is an operator, print close parenthesis
+        if (isOperator(root.data)) {
+            System.out.print(")");
+        }
+    }
+
+    // Function to construct an expression tree from the given postfix expression
+    public CustomNode construct(String postfix)
+    {
+        // base case
+        if (postfix == null || postfix.length() == 0) {
+            return null;
+        }
+
+        // create an empty stack to store tree pointers
+        Stack<CustomNode> s = new Stack<>();
+
+        // traverse the postfix expression
+        for (char c: postfix.toCharArray())
+        {
+            // if the current token is an operator
+            if (isOperator(c))
+            {
+                // pop two nodes `x` and `y` from the stack
+                CustomNode x = s.pop();
+                CustomNode y = s.pop();
+
+                // construct a new binary tree whose root is the operator and whose
+                // left and right children point to `y` and `x`, respectively
+                CustomNode node = new CustomNode(c, y, x);
+
+                // push the current node into the stack
+                s.add(node);
+            }
+            // if the current token is an operand, create a new binary tree node
+            // whose root is the operand and push it into the stack
+            else {
+                s.add(new CustomNode(c));
+            }
+        }
+
+        // a pointer to the root of the expression tree remains on the stack
+        return s.peek();
+    }
 }
--- src/LimModifiedExpressionTree/CustomNode.java
+++ src/LimModifiedExpressionTree/CustomNode.java
@@ -1,4 +1,20 @@
 package LimModifiedExpressionTree;
 
 public class CustomNode {
+
+    char data;
+    CustomNode left, right;
+
+    CustomNode(char data)
+    {
+        this.data = data;
+        this.left = this.right = null;
+    }
+
+    CustomNode(char data, CustomNode left, CustomNode right)
+    {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
 }
